818584036d5fffc48a47681ce0122556dbadcaf8
yaxon <yaxon@yaxondeMac-mini-2.local>
2020-05-19 20:25:36 +0800

    first commit

---
+++ TestFramework.framework/Headers/SayHello.h
@@ -0,0 +1,17 @@
+//
+//  SayHello.h
+//  TestFramework
+//
+//  Created by yaxon on 2020/5/19.
+//  Copyright © 2020 yaxon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SayHello : NSObject
+-(void)sayHello;
+@end
+
+NS_ASSUME_NONNULL_END
